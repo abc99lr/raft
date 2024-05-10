f884a1724534b7d51d845fbdfdf5c61480c1211c
abc99lr <abc99lr@gmail.com>
2024-05-10 18:43:55 +0000

    Restore back to use async thrust support.

--- cpp/include/raft/core/resource/resource_types.hpp
+++ cpp/include/raft/core/resource/resource_types.hpp
@@ -42,7 +42,6 @@ enum resource_type {
   STREAM_VIEW,             // view of a cuda stream or a placeholder in
                            // CUDA-free builds
   THRUST_POLICY,           // thrust execution policy
-  THRUST_NOSYNC_POLICY,    // thrust nosync execution policy
   WORKSPACE_RESOURCE,      // rmm device memory resource
   CUBLASLT_HANDLE,         // cublasLt handle
   CUSTOM,                  // runtime-shared default-constructible resource
